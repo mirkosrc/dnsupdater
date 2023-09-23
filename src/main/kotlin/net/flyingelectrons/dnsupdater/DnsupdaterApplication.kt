4b9c612d854c5360ed1a7467179ce39ad1fb4d03
mirkosrc <mirkosrc@posteo.de>
2023-09-23 10:48:06 +0200

    enable scheduling

--- src/main/kotlin/net/flyingelectrons/dnsupdater/DnsupdaterApplication.kt
+++ src/main/kotlin/net/flyingelectrons/dnsupdater/DnsupdaterApplication.kt
@@ -2,9 +2,20 @@ package net.flyingelectrons.dnsupdater
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
-class DnsupdaterApplication
+@EnableScheduling
+class DnsupdaterApplication {
+
+    @Bean
+    fun restTemplate(builder: RestTemplateBuilder): RestTemplate {
+        return builder.build()
+    }
+} 
 
 fun main(args: Array<String>) {
     runApplication<DnsupdaterApplication>(*args)
